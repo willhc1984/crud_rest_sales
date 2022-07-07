4555a3226c0b2d814feaadfff7c023f2db76f43e
unknown <willhc1984@gmail.com>
2022-07-07 13:41:59 -0300

    OrderItem - Associação ManyToMany com atributos extras

--- src/main/java/com/example/spring/config/TestConfig.java
+++ src/main/java/com/example/spring/config/TestConfig.java
@@ -62,6 +62,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat1);
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
